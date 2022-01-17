80a40a9a62d1c1219521ad34324a7c1d67bfe20c
Alex Gola <alex.gola93@gmail.com>
2022-01-17 05:36:16 +0100

    added new reort to export arraties on expiration

--- aries/2022-01-12/script.sql
+++ aries/2022-01-12/script.sql
@@ -8,3 +8,8 @@ WHERE nome  <> 'MANUTENZIONE';
 UPDATE stato_impianto
 SET non_abbonato = 0
 WHERE nome  = 'MANUTENZIONE';
+
+
+
+INSERT INTO `stampante_moduli` VALUES (15, 'SCAD. GARANZIA', 6, b'0', b'1', b'0', b'1', b'1', NULL, NULL, b'1', NOW());
+INSERT INTO `stampante_moduli_formati` (id_modulo, id_documento, id_formato) VALUES (6, 15, 2);
